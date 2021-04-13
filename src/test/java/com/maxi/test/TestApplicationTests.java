9f423fd30d0385467cabd454ed7e61cf17c697db
nikitavish <nickita9999@mail.ru>
2021-04-14 01:30:58 +0300

    Финальные исправления

--- src/test/java/com/maxi/test/TestApplicationTests.java
+++ src/test/java/com/maxi/test/TestApplicationTests.java
@@ -1,11 +1,7 @@
 package com.maxi.test;
 
-import com.maxi.test.services.XmlToDatabaseServiceImpl;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 @SpringBootTest
 class TestApplicationTests {
